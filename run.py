d17c4988fdd6607ec987ad3b45f68e43ee974301
NiclasEriksen <niclaseriksen@gmx.com>
2024-10-10 12:03:40 +0200

    Verbose logging

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 import json
+from tabnanny import verbose
+
 from domeneshop_bots import DNSBot
 
 
 if __name__ == "__main__":
     with open("./config.json", "r") as file:
         config = json.loads(file.read())
-        bot = DNSBot(config)
+        bot = DNSBot(config, verbose=True)
         bot.start()
