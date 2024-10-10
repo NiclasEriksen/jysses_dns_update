5f7d4a6c93ea639c28ca1737f265722cc00d6d07
NiclasEriksen <niclaseriksen@gmx.com>
2024-10-10 12:01:18 +0200

    Rename config

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from domeneshop_bots import DNSBot
 
 
 if __name__ == "__main__":
-    with open("/config.json", "r") as file:
+    with open("./config.json", "r") as file:
         config = json.loads(file.read())
         bot = DNSBot(config)
         bot.start()
